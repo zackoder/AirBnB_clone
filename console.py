2ac3042ccd49a9b40b43b88af8e3138195aecf32
zackoder <besszakaria@gmail.com>
2023-10-16 09:09:28 -0700

    commit

--- console.py
+++ console.py
@@ -3,7 +3,7 @@
 import cmd
 import re
 from shlex import split
-from models import storage
+from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#/usr/bin/python3
 """Defines the FileStorage class."""
 import json
 from models.base_model import BaseModel
